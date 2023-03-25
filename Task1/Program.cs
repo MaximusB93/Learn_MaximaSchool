251568e199c29a4cc2829cf47341c5791b491855
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-03-25 20:39:31 +0300

    Commit Branch2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -7,7 +7,7 @@ namespace Task1
         static void Main(string[] args)
         {
             Console.WriteLine("Мастер");
-            Console.WriteLine("Ветка 1");
+            Console.WriteLine("Ветка 2");
         }
     }
 }
