932d57524414413d69b70ba3d83da88ebc37e567
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-03-26 00:47:50 +0300

    Task 4_1

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Linq;
 
 namespace Project1
 {
     internal class Program
     {
+        //Сумма элементов одномерного массива
         static void Main(string[] args)
         {
-            Console.WriteLine("Мастер");
+            Console.WriteLine("Введите элементы массива через запятую");
+            string[] strArray = Console.ReadLine().Split(',');
+            int[] intArray = new int[strArray.Length];
+            int summ = 0;
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                intArray[i] = Int32.Parse(strArray[i]);
+                summ += intArray[i];
+            }
+            Console.WriteLine($"Сумма элементов массива - {summ}");
+            Console.ReadLine();
         }
     }
 }
