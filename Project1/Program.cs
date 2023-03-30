af6a3508852e9e9e7ee4abbd42233aa25a6f4503
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-03-30 22:29:27 +0300

    Работа с числами готова, осталось доделать со строками

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Net.Http.Json;
 
 namespace Project1
 {
     internal class Program
     {
+        //Работа с числами
         static void Main(string[] args)
         {
-            Console.WriteLine("Мастер");
+            Console.WriteLine("Введите первое число");
+            int firstNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int secondNumber = Int32.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Сложение чисел - {firstNumber + secondNumber}");
+
+            Console.WriteLine($"Вычитание второго числа от первого - {firstNumber - secondNumber}");
+
+            Console.WriteLine($"Умножение чисел - {firstNumber * secondNumber}");
+
+            Console.WriteLine($"Деление первого числа на второе - {firstNumber / secondNumber}");
         }
     }
 }
--- Project2/Program.cs
+++ Project2/Program.cs
@@ -4,9 +4,17 @@ namespace Project2
 {
     class Program
     {
+        //Работа со строками
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string read = Console.ReadLine();
+
+            read = read.Replace(read,"Новая строка");
+            Console.WriteLine(read);
+            Console.WriteLine(read.ToUpper());
+            Console.WriteLine(read.Remove(6));
+            Console.WriteLine(read.Substring(6));
+
         }
     }
 }
