1d2bae2e1cce908f795d320ecbeeac0e918f7745
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-04-02 15:04:58 +0300

    Домашка 3

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 
 namespace Project2
 {
@@ -7,14 +10,27 @@ namespace Project2
         //Работа со строками
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите название фруктов через запятую");
             string read = Console.ReadLine();
 
-            read = read.Replace(read,"Новая строка");
-            Console.WriteLine(read);
-            Console.WriteLine(read.ToUpper());
-            Console.WriteLine(read.Remove(6));
-            Console.WriteLine(read.Substring(6));
+            //Разбивка строки по элементам массива
+            string[] fruits = read.Split(',');
+            for (int i = 0; i < fruits.Length; i++)
+                Console.WriteLine(fruits[i]);
 
+            //Перевод в верхний регистр
+            Console.WriteLine(read.ToUpper());
+
+            //Перезапись строки
+            read = read.Replace(read, "Перезаписали строку");
+            Console.WriteLine(read);
+
+            //Добавили доп слово
+            Console.WriteLine(read.Insert(13, "нашу "));
+            
+            //Удаление части строки
+            Console.WriteLine(read.Remove(12));
+            Console.WriteLine(read.Substring(13));
         }
     }
 }
