792380c2d05b46d9bedc6792e95fdaad1db368d5
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-04-08 14:41:58 +0300

    Рекурсия

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -6,7 +6,25 @@ namespace Project2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N = Int32.Parse(Console.ReadLine());
+            int count = 0;
+            int a = 0;
+            Console.WriteLine(a);
+            int b = 1;
+            Console.WriteLine(b);
+            Recurs(N, a, b, count);
+        }
+
+        static void Recurs(int N, int a, int b, int count)
+        {
+            if (count == N - 2)
+            {
+                return;
+            }
+            Console.WriteLine(b = a + b);
+            a = b - a;
+            count++;
+            Recurs(N, a, b, count);
         }
     }
 }
