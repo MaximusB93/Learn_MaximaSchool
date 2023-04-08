bcca505f3af346b70fba38840c742867efb5c44e
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-04-08 14:42:28 +0300

    Урок 6

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,7 +6,40 @@ namespace Project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Мастер");
+            Recurs(5);
         }
+
+        static int Recurs(int n)
+        {
+
+            /*if (n >= 5)
+            {
+                return;
+            }
+            
+            Console.WriteLine(n);
+            n++;
+            Recurs(n);*/
+            
+            
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return Recurs(n - 1) * n;
+            }
+        }
+
+
+        /*static (string, int, int[]) Print()
+        {
+            string a = "Привет";
+            int b = 0;
+            int[] c = new int[10];
+
+            return (a, b, c);
+        }*/
     }
 }
