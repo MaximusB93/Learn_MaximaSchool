12cb5be18cf7671b58f5f733395f88f79b88a633
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-04-18 14:32:38 +0300

    Готова задача - Числа Фибоначчи

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -1,46 +1,34 @@
 ﻿using System;
-using System.Data;
 
 namespace Project2
 {
+    /// <summary>
+    /// Числа Фибоначчи(https://pas1.ru/fibonacci)
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            /*int N = Int32.Parse(Console.ReadLine());
-            int count = 0;
+            Console.WriteLine("Введите количество чисел Фибоначчи");
             int a = 0;
-            Console.WriteLine(a);
             int b = 1;
-            Console.WriteLine(b);
-            Recurs(N, a, b, count);*/
-            int a = 1;
-            int N = Int32.Parse(Console.ReadLine());
-            Recurs2(N, a);
+            int count = 0;
+            Console.Write($"{a}, {b}, ");
+            RecursSequenceFibonacci(Int32.Parse(Console.ReadLine() ?? string.Empty), a, b, count);
         }
 
-        static void Recurs2(int N, int a)
+        static void RecursSequenceFibonacci(int n, int a, int b, int count)
         {
-            if (a > N)
-            {
+            if (count == n - 2)
                 return;
+            if (count < n - 3)
+            {
+                b = a + b;
+                a = b - a;
+                count++;
+                Console.Write(count < n - 3 ? $"{b}, " : $"{b}");
+                RecursSequenceFibonacci(n, a, b, count);
             }
-
-            Console.Write(a);
-            Recurs2(N, ++a);
         }
-
-
-        /*static void Recurs(int N, int a, int b, int count)
-        {
-            if (count == N - 2)
-            {
-                return;
-            }
-            Console.WriteLine(b = a + b);
-            a = b - a;
-            count++;
-            Recurs(N, a, b, count);
-        }*/
     }
 }
