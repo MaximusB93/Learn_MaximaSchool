2245990c13050990a8c46eab2fb7ba0818590c48
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-04-21 15:33:23 +0300

    Создан класс

---
+++ Project1/Man.cs
@@ -0,0 +1,17 @@
+namespace Project1
+{
+    public class Man
+    {
+        public enum ColorEye
+        {
+            Blue,
+            Green,
+            Brown
+        }
+        
+        private string _name;
+        private byte _age;
+        private int _height;
+        private int _wheight;
+        private ColorEye _color;
+}
