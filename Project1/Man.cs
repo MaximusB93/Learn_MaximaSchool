f89f30d11e43602a28e3c16256fdc7f62ea0a128
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-04-22 17:51:32 +0300

    Работа с классом Man

--- Project1/Man.cs
+++ Project1/Man.cs
@@ -1,18 +1,76 @@
+using System;
+
 namespace Project1
 {
     public class Man
     {
-        public enum ColorEye
+        public enum Color
         {
             Blue,
             Green,
             Brown,
             Brown2
         }
-        
+
+        private static int _number;
+
+        public static int GetNumber()
+        {
+            return _number;
+        }
+
         private string _name;
         private byte _age;
         private int _height;
         private int _wheight;
-        private ColorEye _color;
+        private Color _eyecolor;
+
+        public Man(string name, byte age, int height, int wheight)
+        {
+            if (!Check(age, height, wheight))
+            {
+                throw new Exception("Некорректные данные");
+            }
+
+            _number++;
+            _name = name;
+            _age = age;
+            _height = height;
+            _wheight = wheight;
+        }
+
+        public Man(string name, byte age, int height, int wheight, Color eyecolor)
+            : this(name, age, height, wheight)
+        {
+            _eyecolor = eyecolor;
+        }
+
+
+        public static int Number => _number;
+
+
+        public byte Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        private bool Check(byte age, int height, int wheight)
+        {
+            if (age > 120)
+                return false;
+            if (height > 300 || height <= 0)
+                return false;
+            if (wheight <= 0)
+                return false;
+
+            return true;
+        }
+
+
+        public string Print()
+        {
+            return $"{_name}, {_age} лет, рост {_height}";
+        }
+    }
 }
--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,7 +6,17 @@ namespace Project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Мастер2");
+            var Ivan = new Man("Ваня", 10, 140, 50, Man.Color.Blue);
+            var Ivan1 = new Man("Ваня", 10, 140, 50, Man.Color.Blue);
+            var Ivan2 = new Man("Ваня", 10, 140, 50, Man.Color.Blue);
+            var Ivan3 = new Man("Ваня", 10, 140, 50, Man.Color.Blue);
+            
+            
+            
+            Console.WriteLine(Ivan.Age);
+            Ivan.Age = 99;
+            Console.WriteLine(Ivan.Age);
+            Console.WriteLine(Man.GetNumber());
         }
     }
 }
