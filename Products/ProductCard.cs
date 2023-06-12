923a296cfc55c2951ef50e92c8abafc5ef8ac154
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-06-12 16:07:30 +0300

    Delegate

--- Products/ProductCard.cs
+++ Products/ProductCard.cs
@@ -6,16 +6,50 @@ namespace Products
 {
     public class ProductCard
     {
+        
+        
         public List<Product> Items { get; }
 
-        public ProductCard()
+        public delegate void NotifyAdddedProduct(Product product);
+
+        private readonly NotifyAdddedProduct _notifyAdddedProduct;
+
+        public ProductCard(NotifyAdddedProduct notifyAdddedProduct)
         {
             Items = new List<Product>();
+            _notifyAdddedProduct = notifyAdddedProduct;
         }
 
-        public void AddProductCard(Product product)
+        public void AddProductCard(Product product )
         {
             Items.Add(product);
+            _notifyAdddedProduct(product);
+        }
+ 
+        public decimal GetTotalSumm()
+        {
+            decimal summ = 0;
+            foreach (var product in Items)
+            {
+                summ += product.Price;
+            }
+
+            if (summ > 1000)
+            {
+                return summ * 095m;
+            }
+
+            if (summ > 100)
+            {
+                return summ * 0975m;
+            }
+
+            if (summ > 25)
+            {
+                return summ * 099m;
+            }
+
+            return summ;
         }
 
         public string PrintAllProducts()
