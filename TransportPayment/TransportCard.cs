5206502bc32f12cafd7ce489870b5a1ccf1e461f
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-07-02 13:55:56 +0300

    Done DZ by Delegates

--- TransportPayment/TransportCard.cs
+++ TransportPayment/TransportCard.cs
@@ -1,27 +1,29 @@
 using System;
-using System.Collections.Generic;
 
 namespace TransportPayment
 {
     public partial class TransportCard
     {
-        public delegate void OnAddMoney(decimal count, decimal balance);
-        public static OnAddMoney onAddMoney;
-        public delegate string NotifyOperation(decimal amount, decimal balance);
+        public delegate void NotifyOperationDelegate(decimal amount, decimal balance);
 
-        public NotifyOperation notifyOperation;
-        public event NotifyOperation notifyOperationEvent; //разобраться
+        public NotifyOperationDelegate NotifyOperation;
 
-        public Func<decimal, decimal, string> notifyCashback;
-        public Predicate<decimal> predicate; //разобраться
-        
+        public Action<decimal> NotifyError;
+
+        public Action<decimal, decimal> NotifyCashback;
         public decimal Balance { get; set; }
 
-        public decimal Add(decimal amount)
+        public decimal DepositАmount()
+        {
+            Console.WriteLine("Введите сумму");
+            decimal amount = decimal.Parse(Console.ReadLine() ?? string.Empty);
+            return amount;
+        }
+
+        public void Add(decimal amount)
         {
             Balance += amount;
-            onAddMoney?.Invoke(amount, Balance);
-            return Balance;
+            NotifyOperation?.Invoke(amount, Balance);
         }
     }
 }
