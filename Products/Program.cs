0b82a9f0f0efc60124a45312c42765587237d46f
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-07-17 21:55:04 +0300

    LINQ2

--- Products/Program.cs
+++ Products/Program.cs
@@ -19,36 +19,57 @@ namespace Products
 
         static void Main()
         {
-            //     ● Дано целое число K (> 0) и целочисленная последовательность A. Найти теоретико-множественную
-            // разность двух фрагментов A: первый содержит все четные числа, а второй — все числа с порядковыми
-            //     номерами, большими K. В полученной последовательности (не содержащей одинаковых элементов)
-            // поменять порядок элементов на обратный.
-            //     ● Даны целые числа K1 и K2 и целочисленные последовательности A и B. Получить последовательность,
+
+
+            // ● Даны целые числа K1 и K2 и целочисленные последовательности A и B. Получить последовательность,
             //     содержащую все числа из A, большие K1, и все числа из B, меньшие K2. Отсортировать полученную
             // последовательность по возрастанию.
+
+            int K1 = 3;
+            int K2 = 5;
+
+            var arrayAA = new[] { 1, 2,7,6,3,4,85,48,473,146 };
+            var arrayBB = new[] { 7,6,2,45,4654,4,1641,21, };
+
+            var aaa = arrayAA.Where(x => x > K1).ToList();
+            var bbb = arrayBB.Where(x => x < K2).ToList();
+            var ddd = aaa.Concat(bbb).ToList();
+            
+            List<int> ccc = new List<int>();
+                ccc.AddRange(aaa);
+                ccc.AddRange(bbb);
+            
+                return;
+            
             //     ● Исходная последовательность содержит сведения об абитуриентах. Каждый элемент
             // последовательности включает следующие поля:
             //     <Фамилия> <Год поступления> <Номер школы>
             //     Для каждой школы вывести общее число абитуриентов за все годы и фамилию первого из абитуриентов
             // этой школы, содержащихся в исходном наборе данных (вначале указывать номер школы, затем число
             //     абитуриентов, затем фамилию). Сведения о каждой школе выводить на новой строке и упорядочивать
-            // по возрастанию номеров школ.
+            // по возрастанию номеров школ.*/
 
-
+            
+            
+            // Дано целое число K (> 0) и целочисленная последовательность A. Найти теоретико-множественную
+            // разность двух фрагментов A: первый содержит все четные числа, а второй — все числа с порядковыми
+            //     номерами, большими K. В полученной последовательности (не содержащей одинаковых элементов)
+            // поменять порядок элементов на обратный.
             int K = 7;
             Random random = new Random();
-            var a = Enumerable.Range(0, 10)
-                .Select(x => random.Next(1, 50))
+            var arrayA = Enumerable.Range(0, 10)
                 .ToArray();
 
+            var arrayA1 = arrayA.Where(x => x % 2 == 0).ToArray();
+            var arrayA2 = arrayA.Where(x => x > K);
+            var differenceA = arrayA1.Except(arrayA2).ToArray();
 
-            IEnumerable aaa = a.Where(x => x % 2 == 0).ToArray();
-            var bbb = aaa.Except(a.Skip(K + 1));
 
+            return;
 
-            var milk = new Product(60, "Milk");
-            var cheese = new Product(75, "Cheese");
-            var curd = new Product(90, "Curd");
+            var milk = new Product(60, "item_milk");
+            var cheese = new Product(75, "item_cheese");
+            var curd = new Product(90, "item_curd");
 
             var bananas = new Product(90, "Bananas");
             var apples = new Product(120, "Apples");
@@ -61,19 +82,54 @@ namespace Products
 
             var milkProduct = new ProductCard(new List<Product>() { milk, cheese, curd }, "Молочная продукция");
             var fruits = new ProductCard(new List<Product>() { bananas, apples, pears, orange }, "Фрукты");
-
-            var linq = milkProduct.Products.Intersect(fruits.Products).ToList();
-            var linq1 = milkProduct.Products.Except(fruits.Products).ToList();
-            var linq2 = milkProduct.Products.Union(fruits.Products).ToList();
-            var linq3 = milkProduct.Products.Concat(fruits.Products).ToList();
-            var linq4 = milkProduct.Products.ElementAtOrDefault(5);
-
             var vegetables = new ProductCard(new List<Product>() { cucumbers, tomatoes, pepper }, "Овощи");
 
             var listCard = new List<ProductCard> { milkProduct, fruits, vegetables };
 
             var dictionary = listCard.ToDictionary(x => x.CategoryName, x => x.Products.Select(x => x.Title));
+
+            var dictionaryLoc = new Dictionary<string, Dictionary<string, string>>()
+            {
+                {
+                    "item_milk", new Dictionary<string, string>()
+                    {
+                        { "ru", "Молоко" },
+                        { "en", "Milk" }
+                    }
+                },
+                {
+                    "item_cheese", new Dictionary<string, string>()
+                    {
+                        { "ru", "Сыр" },
+                        { "en", "Cheese" }
+                    }
+                },
+                {
+                    "item_curd", new Dictionary<string, string>()
+                    {
+                        { "ru", "Творог" },
+                        { "en", "Curd" }
+                    }
+                }
+            };
+
+            // var dictionaryLoc = new Dictionary<string, string>()
+            // {
+            //     { "Milk", "Молоко" },
+            //     { "Cheese", "Сыр" },
+            //     { "Curd", "Творог" }
+            // };
+
+            foreach (var item in milkProduct.Products)
+            {
+                dictionaryLoc[item.Title].TryGetValue("DE", out string a);
+                Console.WriteLine($"{item.Title} - {a}");
+            }
+
+            return;
+
             var dictonary2 = new Dictionary<string, string>();
+
             foreach (var item in dictionary)
             {
                 Console.WriteLine($"Категория: {item.Key}");
@@ -87,30 +143,30 @@ namespace Products
                 Console.WriteLine();
             }
 
-            var dic = listCard.Select(x => x.CategoryName)
-                .ToList();
-
-
-            var nameProductCard = listCard.Select(x => x.CategoryName)
-                .ToList();
-
-
-            var aaaa = listCard.SelectMany(x => x.Products)
-                .ElementAtOrDefault(7);
-
-            var prods = listCard.SelectMany(x => x.Products)
-                .Where(x => x.Price > 50 && x.Price < 100)
-                .OrderBy(x => x.Price)
-                .Select(x => x.Title)
-                .ToList();
-
-            var prods2 = listCard.SelectMany(x => x.Products)
-                .GroupBy(x => x.Price)
-                .Select(x => new { Price = x.Key, Count = x.Count() })
-                .ToList();
-
-            var prods3 = listCard.SelectMany(x => x.Products)
-                .ToList();
+            // var dic = listCard.Select(x => x.CategoryName)
+            //     .ToList();
+            //
+            //
+            // var nameProductCard = listCard.Select(x => x.CategoryName)
+            //     .ToList();
+            //
+            //
+            // var aaaa = listCard.SelectMany(x => x.Products)
+            //     .ElementAtOrDefault(7);
+            //
+            // var prods = listCard.SelectMany(x => x.Products)
+            //     .Where(x => x.Price > 50 && x.Price < 100)
+            //     .OrderBy(x => x.Price)
+            //     .Select(x => x.Title)
+            //     .ToList();
+            //
+            // var prods2 = listCard.SelectMany(x => x.Products)
+            //     .GroupBy(x => x.Price)
+            //     .Select(x => new { Price = x.Key, Count = x.Count() })
+            //     .ToList();
+            //
+            // var prods3 = listCard.SelectMany(x => x.Products)
+            //     .ToList();
 
 
             return;
