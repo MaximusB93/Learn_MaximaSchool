b81e88d28477766440f0f11b847bf40d6991630e
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-07-24 17:30:54 +0300

    Lock

---
+++ Lock2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Lock
+{
+    class Program
+    {
+        private static object _sync = new object();
+        private static List<int> _list = new List<int>();
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(ReadDelegate) { Name = "Read1" };
+            Thread t2 = new Thread(ReadDelegate) { Name = "Read2" };
+            Thread t3 = new Thread(ReadDelegate) { Name = "Read3" };
+
+            Thread t4 = new Thread(WriteDelegate) { Name = "Write1" };
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            t4.Start();
+
+            Console.ReadLine();
+        }
+
+        private static void ReadDelegate()
+        {
+            while (true)
+            {
+                try
+                {
+                    Monitor.Enter(_sync);
+                    foreach (var item in _list)
+                    {
+                        Console.Write(item);
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(_sync);
+                }
+
+                // lock (_sync)
+                // {
+                //     foreach (var item in _list)
+                //     {
+                //         Console.Write(item);
+                //     }
+                // }
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            }
+        }
+
+        private static void WriteDelegate()
+        {
+            while (true)
+            {
+                lock (_sync)
+                {
+                    _list.Add(1);
+                }
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            }
+        }
+    }
+}
---
+++ SemaphoreExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace SemaphoreExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Reader reader = new Reader(i);
+            }
+        }
+    }
+
+    class Reader
+    {
+        private static Semaphore sem = new Semaphore(3, 3);
+        private Thread myThread;
+        private int count = 3;
+
+        public Reader(int i)
+        {
+            myThread = new Thread(Read);
+            myThread.Name = $"Читатель {i}";
+            myThread.Start();
+        }
+
+        public void Read()
+        {
+            while (count > 0)
+            {
+                sem.WaitOne();
+                Console.WriteLine($"{Thread.CurrentThread.Name} входит в библиотеку");
+                Console.WriteLine($"{Thread.CurrentThread.Name} читает");
+                Thread.Sleep(10000);
+                Console.WriteLine($"{Thread.CurrentThread.Name} покидает библиотеку");
+                sem.Release();
+                count--;
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
