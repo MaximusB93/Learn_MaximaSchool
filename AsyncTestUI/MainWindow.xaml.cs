3feaa7ca7967d029b18942e5ce15bfc7febde120
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-08-09 15:51:48 +0300

    - UI Cancel Task

--- AsyncTestUI/MainWindow.xaml.cs
+++ AsyncTestUI/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,12 +23,22 @@ namespace AsyncTestUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
         private string[] _url = new[]
         {
             "https://yandex.ru",
             "https://google.ru",
             "https://vk.com",
             "https://msdn.com",
+            "https://yandex.ru",
+            "https://google.ru",
+            "https://vk.com",
+            "https://msdn.com",
+            "https://yandex.ru",
+            "https://google.ru",
+            "https://vk.com",
+            "https://msdn.com"
         };
 
         public MainWindow()
@@ -35,10 +46,10 @@ namespace AsyncTestUI
             InitializeComponent();
         }
 
+//var1
         private string LoadAllData()
         {
             StringBuilder sb = new StringBuilder();
-            object _sync = new object();
             var time = Stopwatch.StartNew();
 
             //var1
@@ -53,13 +64,14 @@ namespace AsyncTestUI
             return sb.ToString();
         }
 
+//var1
         private string LoadAllDataParallel()
         {
             StringBuilder sb = new StringBuilder();
             object _sync = new object();
             var time = Stopwatch.StartNew();
 
-            //var2
+
             Parallel.ForEach(_url, url =>
             {
                 var result = LoadData(url);
@@ -67,7 +79,62 @@ namespace AsyncTestUI
                 {
                     sb.AppendLine($"URL {url} - {result}");
                 }
+
+                Task.Delay(TimeSpan.FromMilliseconds(0.5));
             });
+
+            time.Stop();
+            sb.AppendLine($"Total time - {time.ElapsedMilliseconds}ms");
+            return sb.ToString();
+        }
+
+//var2
+        private Task<string> LoadAllDataTasks()
+        {
+            return Task.Run(() =>
+            {
+                StringBuilder sb = new StringBuilder();
+                object _sync = new object();
+                var time = Stopwatch.StartNew();
+                var listTasks = new List<Task>();
+                foreach (var url in _url)
+                {
+                    var task = new Task(async () =>
+                    {
+                        var result = await LoadDataAsync(url);
+                        lock (_sync)
+                        {
+                            sb.AppendLine($"URL {url} - {result}");
+                        }
+
+                        Task.Delay(TimeSpan.FromMilliseconds(0.5));
+                    });
+                    task.Start();
+                    listTasks.Add(task);
+                }
+
+                Task.WaitAll(listTasks.ToArray());
+                time.Stop();
+                sb.AppendLine($"Total time - {time.ElapsedMilliseconds}ms");
+                return sb.ToString();
+            });
+        }
+
+        private async Task<string> LoadAllDataTasksAsync()
+        {
+            StringBuilder sb = new StringBuilder();
+            var time = Stopwatch.StartNew();
+            foreach (var url in _url)
+            {
+                if (_cancellationTokenSource.IsCancellationRequested)
+                {
+                    sb.AppendLine("Была запрошена отмена");
+                    break;
+                }
+
+                var result = await LoadDataAsync(url);
+                sb.AppendLine($"URL {url} - {result}");
+            }
 
             time.Stop();
             sb.AppendLine($"Total time - {time.ElapsedMilliseconds}ms");
@@ -78,9 +145,34 @@ namespace AsyncTestUI
         {
             var time = Stopwatch.StartNew();
             WebClient webClient = new WebClient();
-            var result = webClient.DownloadString(url);
-            time.Stop();
-            return $"SIZE - {result.Length}. Time - {time.ElapsedMilliseconds}ms";
+            try
+            {
+                var result = webClient.DownloadString(url);
+                time.Stop();
+                return $"SIZE - {result.Length}. Time - {time.ElapsedMilliseconds}ms";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private async Task<string> LoadDataAsync(string url)
+        {
+            var time = Stopwatch.StartNew();
+            WebClient webClient = new WebClient();
+            try
+            {
+                var result = await webClient.DownloadStringTaskAsync(url);
+
+                time.Stop();
+                return $"SIZE - {result.Length}. Time - {time.ElapsedMilliseconds}ms";
+            }
+            catch (WebException e)
+            {
+                return $"Error {url}. {e.Message}";
+            }
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
@@ -91,6 +183,23 @@ namespace AsyncTestUI
         private void Parallel_OnClick(object sender, RoutedEventArgs e)
         {
             StatusLogTextBlock.Text = LoadAllDataParallel();
+        }
+
+        private async void Tasks_OnClick(object sender, RoutedEventArgs e)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            ButtonTask.IsEnabled = false;
+            ButtonCancel.IsEnabled = true;
+
+            StatusLogTextBlock.Text = await LoadAllDataTasksAsync();
+
+            ButtonTask.IsEnabled = true;
+            ButtonCancel.IsEnabled = false;
+        }
+
+        private async void Cancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            _cancellationTokenSource.Cancel();
         }
     }
 }
--- CancelTask/Program.cs
+++ CancelTask/Program.cs
@@ -13,37 +13,53 @@ namespace CancelTask
         {
             _cancellationTokenSource = new CancellationTokenSource();
             Stopwatch stopwatch = new Stopwatch();
-            var task1 = Task.Factory.StartNew(LongWork1, _cancellationTokenSource.Token);
-            var task2 = Task.Factory.StartNew(LongWork2, _cancellationTokenSource.Token);
-            var tasks = new Task<int>[2] { task1, task2 };
-            int finishedIndex = Task.WaitAny(tasks);
-            _cancellationTokenSource.Cancel(true);
-            var result = tasks[finishedIndex];
-            Console.WriteLine($"Result - {result.Result}. finished task index - {finishedIndex}");
 
+            try
+            {
+                var task1 = Task.Factory.StartNew(LongWork1, _cancellationTokenSource.Token);
+                var task2 = Task.Factory.StartNew(LongWork2, _cancellationTokenSource.Token);
 
-            //Console.ReadLine();
+                Task.Factory.ContinueWhenAny(new[] { task1, task2 },
+                    tasks =>
+                    {
+                        Task.Delay(1000);
+                        Console.WriteLine($"Result - {task1.Result}. Second - {stopwatch.ElapsedMilliseconds}");
+                    });
+                // var tasks = new Task<int>[2] { task1, task2 };
+                // int finishedIndex = Task.WaitAny(tasks);
+                // _cancellationTokenSource.Cancel(true);
+                // var result = tasks[finishedIndex];
+                // Console.WriteLine($"Result - {result.Result}. finished task index - {finishedIndex}");
+                // stopwatch.Stop();
+                // Console.WriteLine($"Result - {task1.Result + task2.Result}. Second - {stopwatch.ElapsedMilliseconds}");
+            }
+            catch (AggregateException e)
+            {
+            }
+    
+
+            Console.ReadLine();
         }
 
 
         private static int LongWork1()
         {
             Thread.Sleep(2000);
-            if (_cancellationTokenSource.IsCancellationRequested)
-            {
-                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
-            }
-
+            // if (_cancellationTokenSource.IsCancellationRequested)
+            // {
+            //     _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+            // }
+            Console.WriteLine("Первый поток закончил работу");
             return 5 + 4;
         }
 
         private static int LongWork2()
         {
-            Thread.Sleep(5000);
-            if (_cancellationTokenSource.IsCancellationRequested)
-            {
-                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
-            }
+            Thread.Sleep(10000);
+            // if (_cancellationTokenSource.IsCancellationRequested)
+            // {
+            //     _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+            // }
 
             Console.WriteLine("Второй поток закончил работу");
             return 10;
