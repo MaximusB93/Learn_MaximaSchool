058f585dd4f4c18983252701531702a335eeafb1
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-08-10 21:12:58 +0300

    - Async

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -17,59 +17,61 @@ namespace Project1
 {
     internal class Program
     {
+        static bool execute = false;
+        private static object _sync = new object();
+
         private static string[] _url = new[]
         {
-            "https://yandex.ru",
             "https://google.com",
             "https://vk.com",
-            "https://msdn.com"
+            "https://msdn.com",
+            "https://yandex.ru",
+            "https://youtube.com",
+            "https://ya.ru"
         };
 
         private static CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private static CancellationToken _token = _cancellationTokenSource.Token;
 
         static void Main(string[] args)
         {
-            GetResponce().Wait();
-        }
-
-        public static async Task<string> GetResponce()
-        {
-            Stopwatch time = Stopwatch.StartNew();
-            StringBuilder sb = new StringBuilder();
-
             foreach (var url in _url)
             {
-                if (_cancellationTokenSource.IsCancellationRequested)
-                {
-                    sb.AppendLine("Была запрошена отмена");
-                    break;
-                }
-
-                var result = await LoadAllData(url);
-                Console.WriteLine(sb.AppendLine($"{url} - {result}"));
+                LoadAllData(url);
             }
 
-            time.Stop();
-            Task.Delay(1000);
-            Console.WriteLine(sb.AppendLine($"Total time - {time.ElapsedMilliseconds}ms"));
-            return sb.ToString();
+            Console.ReadLine();
         }
 
-        public static async Task<string> LoadAllData(string url)
+        public static async Task LoadAllData(string url)
         {
             var time = Stopwatch.StartNew();
             WebClient webClient = new WebClient();
             try
             {
                 var body = await webClient.DownloadStringTaskAsync(url);
-                _cancellationTokenSource.Cancel();
                 time.Stop();
-                return $"SIZE - {body.Length}. Time - {time.ElapsedMilliseconds}ms";
+                Finish(url, $"SIZE - {body.Length}. Time - {time.ElapsedMilliseconds}ms");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
+            }
+        }
+
+        public static void Finish(string url, string data)
+        {
+            lock (_sync)
+            {
+                if (_token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Была запрошена отмена");
+                    return;
+                }
+
+                _cancellationTokenSource.Cancel();
+                Console.WriteLine($"{url} - {data}");
             }
         }
     }
