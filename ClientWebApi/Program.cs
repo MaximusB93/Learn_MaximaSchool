8ac3d01fb1c2f42aa8a2eca9978f56c198761413
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-09-27 09:23:57 +0300

    Api

---
+++ ClientWebApi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientWebApi
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var http = new HttpClient();
+            var url = "http://localhost:5085/WeatherForecast";
+            var response = await http.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+           
+        }
+    }
+}
+ 
